a7140d4bed0938dce19b62740087bfc76fd8d2ea
supernova0216 <150007006+supernova0216@users.noreply.github.com>
2026-03-12 23:17:55 -0700

    push main files

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from db import db   #import the SQLAlchemy instance from db.py
+from user.models import Users, Devices, Metadata, Alerts   #import models to create tables
+from user.routes import user_bp   #import user blueprint for routes
+from dotenv import load_dotenv      #load environment variables from .env file
+import os           #access environment variables
+
+load_dotenv()   
+app = Flask(__name__)   #create a flask app instance
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE")       #configure the database URI  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    #track modifications setting
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")    #set secret key for session management
+app.register_blueprint(user_bp)        #register user blueprint
+
+db.init_app(app)   #initialize SQLAlchemy instance with app
+with app.app_context():
+    db.create_all()
+    print('Created database!')
+
+#define routes for different pages
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':   
+    app.run(debug=True)
